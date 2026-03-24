c59dac4b73585fd8ecea0ba9d8a5b11dc460b1ee
AB45699 <b.anam8541@gmail.com>
2026-03-24 19:56:07 +0000

    object shape assertions

--- backend/__tests__/app.test.js
+++ backend/__tests__/app.test.js
@@ -39,6 +39,40 @@ describe("app", ()=>{
             const { body } = await request(app).get("/api/restaurants"); 
             
             expect(body.restaurants.length).toBe(10);
+        });
+        test("the restaurants (objects) within the array have the required id, name, address, rating and cuisine keys", async ()=>{
+            global.fetch.mockResolvedValueOnce({
+                ok: true, 
+                json: async () => mockData
+            });
+
+            const { body } = await request(app).get("/api/restaurants");
+
+            body.restaurants.forEach((restaurant)=>{
+                expect(restaurant).toHaveProperty("id");
+                expect(typeof(restaurant.id)).toBe("string");
+
+                expect(restaurant).toHaveProperty("name");
+                expect(typeof(restaurant.name)).toBe("string");
+
+                expect(restaurant).toHaveProperty("address.city");
+                expect(typeof(restaurant.address.city)).toBe("string");
+                expect(restaurant).toHaveProperty("address.firstLine");
+                expect(typeof(restaurant.address.firstLine)).toBe("string");
+                
+                expect(restaurant).toHaveProperty("rating.starRating");
+                expect(typeof(restaurant.rating.starRating)).toBe("number");
+
+                expect(restaurant).toHaveProperty("cuisines");
+                expect(Array.isArray(restaurant.cuisines)).toBe(true);
+                
+                restaurant.cuisines.forEach((cuisine)=>{
+                    expect(typeof(cuisine)).toBe("object");
+                    expect(Array.isArray(cuisine)).toBe(false);
+                    expect(cuisine).toHaveProperty("name");
+                    expect(typeof(cuisine.name)).toBe("string");
+                })
+            });
         })
     })
 })
