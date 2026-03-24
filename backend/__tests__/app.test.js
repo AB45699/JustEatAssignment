378f12acc5e5577aa32ade46029213c143484090
AB45699 <b.anam8541@gmail.com>
2026-03-24 18:08:24 +0000

    confirming response shape before implementing mocks

--- backend/__tests__/app.test.js
+++ backend/__tests__/app.test.js
@@ -5,6 +5,11 @@ describe("app", ()=>{
     describe("GET /api/restaurants", ()=>{
         test("should respond with a status of 200", async ()=>{
             await request(app).get("/api/restaurants").expect(200);
+        });
+        test("responds with an array on the key of restaurants", async ()=> {
+            const { body } = await request(app).get("/api/restaurants"); 
+
+            expect(Array.isArray(body.restaurants)).toBe(true);
         })
     })
 })
--- backend/controllers/restaurants.js
+++ backend/controllers/restaurants.js
@@ -1,3 +1,7 @@
-exports.getRestaurants = (req, res, next) => {
-    res.sendStatus(200);
+exports.getRestaurants = async (req, res, next) => {
+    const response = await fetch(`https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF`)
+    
+    const data = await response.json();
+
+    res.status(200).send({restaurants: data.restaurants});
 }
