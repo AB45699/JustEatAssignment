c97e43ed9e323fbb44f649339ffa6b857083c193
AB45699 <b.anam8541@gmail.com>
2026-03-24 18:19:44 +0000

    implementing fetch mocks

--- backend/__tests__/app.test.js
+++ backend/__tests__/app.test.js
@@ -1,12 +1,31 @@
 const app = require("../app.js"); 
 const request = require("supertest");
+const mockData = require("./mockRestaurantData.js");
+
+beforeEach(()=>{
+    global.fetch = jest.fn();
+}); 
+
+afterEach(()=>{
+    jest.clearAllMocks();
+});
 
 describe("app", ()=>{
     describe("GET /api/restaurants", ()=>{
         test("should respond with a status of 200", async ()=>{
+            global.fetch.mockResolvedValueOnce({
+                ok: true, 
+                json: async () => mockData
+            }); 
+            
             await request(app).get("/api/restaurants").expect(200);
         });
         test("responds with an array on the key of restaurants", async ()=> {
+            global.fetch.mockResolvedValueOnce({
+                ok: true, 
+                json: async () => mockData
+            }); 
+
             const { body } = await request(app).get("/api/restaurants"); 
 
             expect(Array.isArray(body.restaurants)).toBe(true);
