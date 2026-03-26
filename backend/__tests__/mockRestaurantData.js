f7257a5a5b303b16e853b5370b8da09d5cd9c91e
AB45699 <b.anam8541@gmail.com>
2026-03-26 15:59:52 +0000

    removing id field

--- backend/__tests__/mockRestaurantData.js
+++ backend/__tests__/mockRestaurantData.js
@@ -1,77 +1,66 @@
 module.exports = { 
     "restaurants": [
         {
-            "id": "1",
             "name": "Pizza Restaurant",
             "address": {"city": "London","firstLine": "1 London Street"},
             "rating": {"starRating": 3.25},
             "cuisines": [{"name": "Pizza"},{"name": "Italian"}]
         },
         {
-            "id": "2",
             "name": "Burrito Restaurant",
             "address": {"city": "London","firstLine": "2 London Street"},
             "rating": {"starRating": 5},
             "cuisines": [{"name": "Fast food"},{"name": "Mexican"}]
         },
         {
-            "id": "3",
             "name": "Chicken Restaurant",
             "address": {"city": "London","firstLine": "3 London Street"},
             "rating": {"starRating": 3.5},
             "cuisines": [{"name": "Chicken"}]
         },
         {
-            "id": "4",
             "name": "Kebab Restaurant",
             "address": {"city": "London","firstLine": "4 London Street"},
             "rating": {"starRating": 4},
             "cuisines": [{"name": "Kebab"}, {"name": "Pizza"}, {"name": "Deals"}]
         }, 
         {
-            "id": "5",
             "name": "Pizza Restaurant",
             "address": {"city": "London","firstLine": "5 London Street"},
             "rating": {"starRating": 3.25},
             "cuisines": [{"name": "Pizza"},{"name": "Italian"}]
         },
         {
-            "id": "6",
             "name": "Burrito Restaurant",
             "address": {"city": "London","firstLine": "6 London Street"},
             "rating": {"starRating": 5},
             "cuisines": [{"name": "Fast food"},{"name": "Mexican"}]
         },
         {
-            "id": "7",
             "name": "Chicken Restaurant",
             "address": {"city": "London","firstLine": "7 London Street"},
             "rating": {"starRating": 3.5},
             "cuisines": [{"name": "Chicken"}]
         },
         {
-            "id": "8",
             "name": "Kebab Restaurant",
             "address": {"city": "London","firstLine": "8 London Street"},
             "rating": {"starRating": 4},
             "cuisines": [{"name": "Kebab"}, {"name": "Pizza"}, {"name": "Deals"}]
         },
         {
-            "id": "9",
             "name": "Burrito Restaurant",
             "address": {"city": "London","firstLine": "9 London Street"},
             "rating": {"starRating": 5},
             "cuisines": [{"name": "Fast food"},{"name": "Mexican"}]
         },
         {
-            "id": "10",
             "name": "Chicken Restaurant",
             "address": {"city": "London","firstLine": "10 London Street"},
             "rating": {"starRating": 3.5},
             "cuisines": [{"name": "Chicken"}]
         },
         {
-            "id": "11",
             "name": "Kebab Restaurant",
             "address": {"city": "London","firstLine": "11 London Street"},
             "rating": {"starRating": 4},
