e0c4966c16b73da57d507fa08a7ef60ee1ff8069
AB45699 <b.anam8541@gmail.com>
2026-03-27 17:19:23 +0000

    search page styling

--- frontend/src/components/PostcodeInput/PostcodeInput.jsx
+++ frontend/src/components/PostcodeInput/PostcodeInput.jsx
@@ -3,17 +3,17 @@ import './PostcodeInput.css';
 function PostcodeInput() {
 return (
     <div className="form">
-        <h1 className="form__title">Order a takeaway...</h1>
-        <h2 className="form__subtitle">Find restaurants delivering near you</h2>
+        <h1 className="form__title">Order a takeaway</h1>
+        <h2 className="form__subtitle">Find restaurants delivering near you...</h2>
             <div className="form__postcode-input-container">
-            <label htmlFor="postcode-input" className="form__label"> Search for a postcode: </label>
-            <input
-                id = "postcode-input"
-                type="text"
-                className="form__postcode-input"
-                placeholder = "Enter postcode e.g. EC4M 7RF"
-            />
-            <button type="button">Search</button>
+               <label htmlFor="postcode-input" className="form__label"> Search for a postcode: </label>
+               <input
+                   id = "postcode-input"
+                   type="text"
+                   className="form__postcode-input"
+                   placeholder = "Enter postcode"
+                />
+                <button type="button" className="form__search-button">Search</button>
             </div>
     </div>
 )
