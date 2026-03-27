7ad05da36662214e2e17661a48f5f6a5f5f4f967
AB45699 <b.anam8541@gmail.com>
2026-03-27 16:42:42 +0000

    app font stylings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './components/Header/Header.jsx';
+import PostcodeInput from './components/PostcodeInput/PostcodeInput.jsx';
 import './App.css'; 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <PostcodeInput />
     </div>
   )
 };
---
+++ frontend/src/components/PostcodeInput/PostcodeInput.jsx
@@ -0,0 +1,22 @@
+import './PostcodeInput.css'; 
+
+function PostcodeInput() {
+return (
+    <div className="form">
+        <h1 className="form__title">Order a takeaway...</h1>
+        <h2 className="form__subtitle">Find restaurants delivering near you</h2>
+            <div className="form__postcode-input-container">
+            <label htmlFor="postcode-input" className="form__label"> Search for a postcode: </label>
+            <input
+                id = "postcode-input"
+                type="text"
+                className="form__postcode-input"
+                placeholder = "Enter postcode e.g. EC4M 7RF"
+            />
+            <button type="button">Search</button>
+            </div>
+    </div>
+)
+}; 
+
+export default PostcodeInput;
